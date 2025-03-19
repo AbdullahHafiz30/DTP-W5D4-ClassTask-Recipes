cf3942ab8317675b4a5c56e7dafe3d72f31c6226
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-20 02:08:21 +0300

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Abdullah Hafiz on 20/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
